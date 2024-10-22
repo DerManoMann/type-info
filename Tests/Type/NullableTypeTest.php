Sorbetesss/lab1

[TypeInfo] Redesign Type methods and nullability

src/Symfony/Component/TypeInfo/Tests/Type/NullableTypeTest.php
@@ -1,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\TypeInfo\Tests\Type;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\TypeInfo\Exception\InvalidArgumentException;
+use Symfony\Component\TypeInfo\Type;
+use Symfony\Component\TypeInfo\Type\NullableType;
+
+class NullableTypeTest extends TestCase
+{
+    public function testCannotCreateWithNullableType()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new NullableType(Type::null());
+    }
+
+    public function testNullPartIsAdded()
+    {
+        $type = new NullableType(Type::int());
+        $this->assertEquals([Type::int(), Type::null()], $type->getTypes());
+
+        $type = new NullableType(Type::union(Type::int(), Type::string()));
+        $this->assertEquals([Type::int(), Type::null(), Type::string()], $type->getTypes());
+    }
+
+    public function testWrappedTypeSatisfy()
+    {
+        $type = new NullableType(Type::int());
+        $this->assertTrue($type->wrappedTypeSatisfy(static fn (Type $t): bool => 'int' === (string) $t));
+
+        $type = new NullableType(Type::string());
+        $this->assertFalse($type->wrappedTypeSatisfy(static fn (Type $t): bool => 'int' === (string) $t));
+    }
+}

src/Symfony/Component/TypeInfo/Type/CollectionType.php
@@ -18,16 +18,16 @@ use Symfony\Component\TypeInfo\TypeIdentifier;
 /**
  * Represents a key/value collection type.
  *
- * It proxies every method to the main type and adds methods related to key and value types.
- *
  * @author Mathias Arlaud <mathias.arlaud@gmail.com>
  * @author Baptiste Leduc <baptiste.leduc@gmail.com>
  *
  * @template T of BuiltinType<TypeIdentifier::ARRAY>|BuiltinType<TypeIdentifier::ITERABLE>|ObjectType|GenericType
  *
+ * @implements WrappingTypeInterface<T>
+ *
  * @experimental
  */
-final class CollectionType extends Type
+final class CollectionType extends Type implements WrappingTypeInterface
 {
     /**
      * @param T $type
@@ -36,6 +36,10 @@ final class CollectionType extends Type
         private readonly BuiltinType|ObjectType|GenericType $type,
         private readonly bool $isList = false,
     ) {
+        if ($type instanceof BuiltinType && TypeIdentifier::ARRAY !== $type->getTypeIdentifier() && TypeIdentifier::ITERABLE !== $type->getTypeIdentifier()) {
+            throw new InvalidArgumentException(\sprintf('Cannot create "%s" with "%s" type.', self::class, $type));
+        }
+
         if ($this->isList()) {
             $keyType = $this->getCollectionKeyType();
 
@@ -45,32 +49,14 @@ final class CollectionType extends Type
         }
     }
 
-    public function getBaseType(): BuiltinType|ObjectType
-    {
-        return $this->getType()->getBaseType();
-    }
-
-    /**
-     * @return T
-     */
-    public function getType(): BuiltinType|ObjectType|GenericType
+    public function getWrappedType(): Type
     {
         return $this->type;
-    }
-
-    public function isA(TypeIdentifier|string $subject): bool
-    {
-        return $this->getType()->isA($subject);
     }
 
     public function isList(): bool
     {
         return $this->isList;
-    }
-
-    public function asNonNullable(): self
-    {
-        return $this;
     }
 
     public function getCollectionKeyType(): Type
@@ -103,18 +89,13 @@ final class CollectionType extends Type
         return $defaultCollectionValueType;
     }
 
+    public function wrappedTypeSatisfy(callable $specification): bool
+    {
+        return $this->getWrappedType()->satisfy($specification);
+    }
+
     public function __toString(): string
     {
         return (string) $this->type;
-    }
-
-    /**
-     * Proxies all method calls to the original type.
-     *
-     * @param list<mixed> $arguments
-     */
-    public function __call(string $method, array $arguments): mixed
-    {
-        return $this->type->{$method}(...$arguments);
     }
 }

src/Symfony/Component/TypeInfo/Type/GenericType.php
@@ -11,22 +11,23 @@
 
 namespace Symfony\Component\TypeInfo\Type;
 
+use Symfony\Component\TypeInfo\Exception\InvalidArgumentException;
 use Symfony\Component\TypeInfo\Type;
 use Symfony\Component\TypeInfo\TypeIdentifier;
 
 /**
  * Represents a generic type, which is a type that holds variable parts.
  *
- * It proxies every method to the main type and adds methods related to variable types.
- *
  * @author Mathias Arlaud <mathias.arlaud@gmail.com>
  * @author Baptiste Leduc <baptiste.leduc@gmail.com>
  *
  * @template T of BuiltinType<TypeIdentifier::ARRAY>|BuiltinType<TypeIdentifier::ITERABLE>|ObjectType
  *
+ * @implements WrappingTypeInterface<T>
+ *
  * @experimental
  */
-final class GenericType extends Type
+final class GenericType extends Type implements WrappingTypeInterface
 {
     /**
      * @var list<Type>
@@ -40,30 +41,16 @@ final class GenericType extends Type
         private readonly BuiltinType|ObjectType $type,
         Type ...$variableTypes,
     ) {
+        if ($type instanceof BuiltinType && TypeIdentifier::ARRAY !== $type->getTypeIdentifier() && TypeIdentifier::ITERABLE !== $type->getTypeIdentifier()) {
+            throw new InvalidArgumentException(\sprintf('Cannot create "%s" with "%s" type.', self::class, $type));
+        }
+
         $this->variableTypes = $variableTypes;
     }
 
-    public function getBaseType(): BuiltinType|ObjectType
-    {
-        return $this->getType();
-    }
-
-    /**
-     * @return T
-     */
-    public function getType(): BuiltinType|ObjectType
+    public function getWrappedType(): Type
     {
         return $this->type;
-    }
-
-    public function isA(TypeIdentifier|string $subject): bool
-    {
-        return $this->getType()->isA($subject);
-    }
-
-    public function asNonNullable(): self
-    {
-        return $this;
     }
 
     /**
@@ -72,6 +59,11 @@ final class GenericType extends Type
     public function getVariableTypes(): array
     {
         return $this->variableTypes;
+    }
+
+    public function wrappedTypeSatisfy(callable $specification): bool
+    {
+        return $this->getWrappedType()->satisfy($specification);
     }
 
     public function __toString(): string
@@ -86,15 +78,5 @@ final class GenericType extends Type
         }
 
         return $typeString.'<'.$variableTypesString.'>';
-    }
-
-    /**
-     * Proxies all method calls to the original type.
-     *
-     * @param list<mixed> $arguments
-     */
-    public function __call(string $method, array $arguments): mixed
-    {
-        return $this->type->{$method}(...$arguments);
     }
 }

src/Symfony/Component/TypeInfo/Type/IntersectionType.php
@@ -11,27 +11,70 @@
 
 namespace Symfony\Component\TypeInfo\Type;
 
-use Symfony\Component\TypeInfo\Exception\LogicException;
+use Symfony\Component\TypeInfo\Exception\InvalidArgumentException;
 use Symfony\Component\TypeInfo\Type;
 
 /**
  * @author Mathias Arlaud <mathias.arlaud@gmail.com>
  * @author Baptiste Leduc <baptiste.leduc@gmail.com>
  *
- * @template T of Type
+ * @template T of ObjectType|GenericType<ObjectType>|CollectionType<GenericType<ObjectType>>
+ *
+ * @implements CompositeTypeInterface<T>
  *
  * @experimental
  */
-final class IntersectionType extends Type
+final class IntersectionType extends Type implements CompositeTypeInterface
 {
     /**
-     * @use CompositeTypeTrait<T>
+     * @var list<T>
      */
-    use CompositeTypeTrait;
+    private readonly array $types;
 
-    public function is(callable $callable): bool
+    /**
+     * @param list<T> $types
+     */
+    public function __construct(Type ...$types)
     {
-        return $this->everyTypeIs($callable);
+        if (\count($types) < 2) {
+            throw new InvalidArgumentException(\sprintf('"%s" expects at least 2 types.', self::class));
+        }
+
+        foreach ($types as $type) {
+            if ($type instanceof CompositeTypeInterface || $type instanceof NullableType) {
+                throw new InvalidArgumentException(\sprintf('Cannot set "%s" as a "%s" part.', $type, self::class));
+            }
+
+            while ($type instanceof WrappingTypeInterface) {
+                $type = $type->getWrappedType();
+            }
+
+            if (!$type instanceof ObjectType) {
+                throw new InvalidArgumentException(\sprintf('Cannot set "%s" as a "%s" part.', $type, self::class));
+            }
+        }
+
+        usort($types, fn (Type $a, Type $b): int => (string) $a <=> (string) $b);
+        $this->types = array_values(array_unique($types));
+    }
+
+    /**
+     * @return list<T>
+     */
+    public function getTypes(): array
+    {
+        return $this->types;
+    }
+
+    public function composedTypesSatisfy(callable $specification): bool
+    {
+        foreach ($this->types as $type) {
+            if (!$type->satisfy($specification)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public function __toString(): string
@@ -40,30 +83,10 @@ final class IntersectionType extends Type
         $glue = '';
 
         foreach ($this->types as $t) {
-            $string .= $glue.($t instanceof UnionType ? '('.$t.')' : $t);
+            $string .= $glue.($t instanceof CompositeTypeInterface ? '('.$t.')' : $t);
             $glue = '&';
         }
 
         return $string;
-    }
-
-    /**
-     * @throws LogicException
-     */
-    public function getBaseType(): BuiltinType|ObjectType
-    {
-        throw new LogicException(\sprintf('Cannot get base type on "%s" compound type.', $this));
-    }
-
-    /**
-     * @throws LogicException
-     */
-    public function asNonNullable(): self
-    {
-        if ($this->isNullable()) {
-            throw new LogicException(\sprintf('"%s cannot be turned as non nullable.', (string) $this));
-        }
-
-        return $this;
     }
 }

src/Symfony/Component/TypeInfo/Type/NullableType.php
@@ -1,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\TypeInfo\Type;
+
+use Symfony\Component\TypeInfo\Exception\InvalidArgumentException;
+use Symfony\Component\TypeInfo\Type;
+use Symfony\Component\TypeInfo\TypeIdentifier;
+
+/**
+ * @author Mathias Arlaud <mathias.arlaud@gmail.com>
+ *
+ * @template T of Type
+ *
+ * @extends UnionType<T|BuiltinType<TypeIdentifier::NULL>>
+ *
+ * @implements WrappingTypeInterface<T>
+ *
+ * @experimental
+ */
+final class NullableType extends UnionType implements WrappingTypeInterface
+{
+    /**
+     * @param T $type
+     */
+    public function __construct(
+        private readonly Type $type,
+    ) {
+        if ($type->isNullable()) {
+            throw new InvalidArgumentException(\sprintf('Cannot create a "%s" with "%s" because it is already nullable.', self::class, $type));
+        }
+
+        if ($type instanceof UnionType) {
+            parent::__construct(Type::null(), ...$type->getTypes());
+
+            return;
+        }
+
+        parent::__construct(Type::null(), $type);
+    }
+
+    public function getWrappedType(): Type
+    {
+        return $this->type;
+    }
+
+    public function wrappedTypeSatisfy(callable $specification): bool
+    {
+        return $this->getWrappedType()->satisfy($specification);
+    }
+
+    public function isNullable(): bool
+    {
+        return true;
+    }
+}

src/Symfony/Component/TypeInfo/Type/WrappingTypeInterface.php
@@ -1,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\TypeInfo\Type;
+
+use Symfony\Component\TypeInfo\Type;
+
+/**
+ * Represents a type wrapping another type.
+ *
+ * @author Mathias Arlaud <mathias.arlaud@gmail.com>
+ *
+ * @template T of Type
+ *
+ * @experimental
+ */
+interface WrappingTypeInterface
+{
+    /**
+     * @return T
+     */
+    public function getWrappedType(): Type;
+
+    /**
+     * @param callable(T): bool $specification
+     */
+    public function wrappedTypeSatisfy(callable $specification): bool;
+}

DerManoMann/type-info

Tests/Type/NullableTypeTest.php
@@ -1,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\TypeInfo\Tests\Type;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\TypeInfo\Exception\InvalidArgumentException;
+use Symfony\Component\TypeInfo\Type;
+use Symfony\Component\TypeInfo\Type\NullableType;
+
+class NullableTypeTest extends TestCase
+{
+    public function testCannotCreateWithNullableType()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new NullableType(Type::null());
+    }
+
+    public function testNullPartIsAdded()
+    {
+        $type = new NullableType(Type::int());
+        $this->assertEquals([Type::int(), Type::null()], $type->getTypes());
+
+        $type = new NullableType(Type::union(Type::int(), Type::string()));
+        $this->assertEquals([Type::int(), Type::null(), Type::string()], $type->getTypes());
+    }
+
+    public function testWrappedTypeIsSatisfiedBy()
+    {
+        $type = new NullableType(Type::int());
+        $this->assertTrue($type->wrappedTypeIsSatisfiedBy(static fn (Type $t): bool => 'int' === (string) $t));
+
+        $type = new NullableType(Type::string());
+        $this->assertFalse($type->wrappedTypeIsSatisfiedBy(static fn (Type $t): bool => 'int' === (string) $t));
+    }
+}

Type/CollectionType.php
@@ -18,16 +18,16 @@ use Symfony\Component\TypeInfo\TypeIdentifier;
 /**
  * Represents a key/value collection type.
  *
- * It proxies every method to the main type and adds methods related to key and value types.
- *
  * @author Mathias Arlaud <mathias.arlaud@gmail.com>
  * @author Baptiste Leduc <baptiste.leduc@gmail.com>
  *
  * @template T of BuiltinType<TypeIdentifier::ARRAY>|BuiltinType<TypeIdentifier::ITERABLE>|ObjectType|GenericType
  *
+ * @implements WrappingTypeInterface<T>
+ *
  * @experimental
  */
-final class CollectionType extends Type
+final class CollectionType extends Type implements WrappingTypeInterface
 {
     /**
      * @param T $type
@@ -36,6 +36,10 @@ final class CollectionType extends Type
         private readonly BuiltinType|ObjectType|GenericType $type,
         private readonly bool $isList = false,
     ) {
+        if ($type instanceof BuiltinType && TypeIdentifier::ARRAY !== $type->getTypeIdentifier() && TypeIdentifier::ITERABLE !== $type->getTypeIdentifier()) {
+            throw new InvalidArgumentException(\sprintf('Cannot create "%s" with "%s" type.', self::class, $type));
+        }
+
         if ($this->isList()) {
             $keyType = $this->getCollectionKeyType();
 
@@ -45,32 +49,14 @@ final class CollectionType extends Type
         }
     }
 
-    public function getBaseType(): BuiltinType|ObjectType
-    {
-        return $this->getType()->getBaseType();
-    }
-
-    /**
-     * @return T
-     */
-    public function getType(): BuiltinType|ObjectType|GenericType
+    public function getWrappedType(): Type
     {
         return $this->type;
-    }
-
-    public function isA(TypeIdentifier|string $subject): bool
-    {
-        return $this->getType()->isA($subject);
     }
 
     public function isList(): bool
     {
         return $this->isList;
-    }
-
-    public function asNonNullable(): self
-    {
-        return $this;
     }
 
     public function getCollectionKeyType(): Type
@@ -103,18 +89,13 @@ final class CollectionType extends Type
         return $defaultCollectionValueType;
     }
 
+    public function wrappedTypeIsSatisfiedBy(callable $specification): bool
+    {
+        return $this->getWrappedType()->isSatisfiedBy($specification);
+    }
+
     public function __toString(): string
     {
         return (string) $this->type;
-    }
-
-    /**
-     * Proxies all method calls to the original type.
-     *
-     * @param list<mixed> $arguments
-     */
-    public function __call(string $method, array $arguments): mixed
-    {
-        return $this->type->{$method}(...$arguments);
     }
 }

Type/GenericType.php
@@ -11,22 +11,23 @@
 
 namespace Symfony\Component\TypeInfo\Type;
 
+use Symfony\Component\TypeInfo\Exception\InvalidArgumentException;
 use Symfony\Component\TypeInfo\Type;
 use Symfony\Component\TypeInfo\TypeIdentifier;
 
 /**
  * Represents a generic type, which is a type that holds variable parts.
  *
- * It proxies every method to the main type and adds methods related to variable types.
- *
  * @author Mathias Arlaud <mathias.arlaud@gmail.com>
  * @author Baptiste Leduc <baptiste.leduc@gmail.com>
  *
  * @template T of BuiltinType<TypeIdentifier::ARRAY>|BuiltinType<TypeIdentifier::ITERABLE>|ObjectType
  *
+ * @implements WrappingTypeInterface<T>
+ *
  * @experimental
  */
-final class GenericType extends Type
+final class GenericType extends Type implements WrappingTypeInterface
 {
     /**
      * @var list<Type>
@@ -40,30 +41,16 @@ final class GenericType extends Type
         private readonly BuiltinType|ObjectType $type,
         Type ...$variableTypes,
     ) {
+        if ($type instanceof BuiltinType && TypeIdentifier::ARRAY !== $type->getTypeIdentifier() && TypeIdentifier::ITERABLE !== $type->getTypeIdentifier()) {
+            throw new InvalidArgumentException(\sprintf('Cannot create "%s" with "%s" type.', self::class, $type));
+        }
+
         $this->variableTypes = $variableTypes;
     }
 
-    public function getBaseType(): BuiltinType|ObjectType
-    {
-        return $this->getType();
-    }
-
-    /**
-     * @return T
-     */
-    public function getType(): BuiltinType|ObjectType
+    public function getWrappedType(): Type
     {
         return $this->type;
-    }
-
-    public function isA(TypeIdentifier|string $subject): bool
-    {
-        return $this->getType()->isA($subject);
-    }
-
-    public function asNonNullable(): self
-    {
-        return $this;
     }
 
     /**
@@ -72,6 +59,11 @@ final class GenericType extends Type
     public function getVariableTypes(): array
     {
         return $this->variableTypes;
+    }
+
+    public function wrappedTypeIsSatisfiedBy(callable $specification): bool
+    {
+        return $this->getWrappedType()->isSatisfiedBy($specification);
     }
 
     public function __toString(): string
@@ -86,15 +78,5 @@ final class GenericType extends Type
         }
 
         return $typeString.'<'.$variableTypesString.'>';
-    }
-
-    /**
-     * Proxies all method calls to the original type.
-     *
-     * @param list<mixed> $arguments
-     */
-    public function __call(string $method, array $arguments): mixed
-    {
-        return $this->type->{$method}(...$arguments);
     }
 }

Type/IntersectionType.php
@@ -11,27 +11,70 @@
 
 namespace Symfony\Component\TypeInfo\Type;
 
-use Symfony\Component\TypeInfo\Exception\LogicException;
+use Symfony\Component\TypeInfo\Exception\InvalidArgumentException;
 use Symfony\Component\TypeInfo\Type;
 
 /**
  * @author Mathias Arlaud <mathias.arlaud@gmail.com>
  * @author Baptiste Leduc <baptiste.leduc@gmail.com>
  *
- * @template T of Type
+ * @template T of ObjectType|GenericType<ObjectType>|CollectionType<GenericType<ObjectType>>
+ *
+ * @implements CompositeTypeInterface<T>
  *
  * @experimental
  */
-final class IntersectionType extends Type
+final class IntersectionType extends Type implements CompositeTypeInterface
 {
     /**
-     * @use CompositeTypeTrait<T>
+     * @var list<T>
      */
-    use CompositeTypeTrait;
+    private readonly array $types;
 
-    public function is(callable $callable): bool
+    /**
+     * @param list<T> $types
+     */
+    public function __construct(Type ...$types)
     {
-        return $this->everyTypeIs($callable);
+        if (\count($types) < 2) {
+            throw new InvalidArgumentException(\sprintf('"%s" expects at least 2 types.', self::class));
+        }
+
+        foreach ($types as $type) {
+            if ($type instanceof CompositeTypeInterface || $type instanceof NullableType) {
+                throw new InvalidArgumentException(\sprintf('Cannot set "%s" as a "%s" part.', $type, self::class));
+            }
+
+            while ($type instanceof WrappingTypeInterface) {
+                $type = $type->getWrappedType();
+            }
+
+            if (!$type instanceof ObjectType) {
+                throw new InvalidArgumentException(\sprintf('Cannot set "%s" as a "%s" part.', $type, self::class));
+            }
+        }
+
+        usort($types, fn (Type $a, Type $b): int => (string) $a <=> (string) $b);
+        $this->types = array_values(array_unique($types));
+    }
+
+    /**
+     * @return list<T>
+     */
+    public function getTypes(): array
+    {
+        return $this->types;
+    }
+
+    public function composedTypesAreSatisfiedBy(callable $specification): bool
+    {
+        foreach ($this->types as $type) {
+            if (!$type->isSatisfiedBy($specification)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public function __toString(): string
@@ -40,30 +83,10 @@ final class IntersectionType extends Type
         $glue = '';
 
         foreach ($this->types as $t) {
-            $string .= $glue.($t instanceof UnionType ? '('.$t.')' : $t);
+            $string .= $glue.($t instanceof CompositeTypeInterface ? '('.$t.')' : $t);
             $glue = '&';
         }
 
         return $string;
-    }
-
-    /**
-     * @throws LogicException
-     */
-    public function getBaseType(): BuiltinType|ObjectType
-    {
-        throw new LogicException(\sprintf('Cannot get base type on "%s" compound type.', $this));
-    }
-
-    /**
-     * @throws LogicException
-     */
-    public function asNonNullable(): self
-    {
-        if ($this->isNullable()) {
-            throw new LogicException(\sprintf('"%s cannot be turned as non nullable.', (string) $this));
-        }
-
-        return $this;
     }
 }

Type/NullableType.php
@@ -1,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\TypeInfo\Type;
+
+use Symfony\Component\TypeInfo\Exception\InvalidArgumentException;
+use Symfony\Component\TypeInfo\Type;
+use Symfony\Component\TypeInfo\TypeIdentifier;
+
+/**
+ * @author Mathias Arlaud <mathias.arlaud@gmail.com>
+ *
+ * @template T of Type
+ *
+ * @extends UnionType<T|BuiltinType<TypeIdentifier::NULL>>
+ *
+ * @implements WrappingTypeInterface<T>
+ *
+ * @experimental
+ */
+final class NullableType extends UnionType implements WrappingTypeInterface
+{
+    /**
+     * @param T $type
+     */
+    public function __construct(
+        private readonly Type $type,
+    ) {
+        if ($type->isNullable()) {
+            throw new InvalidArgumentException(\sprintf('Cannot create a "%s" with "%s" because it is already nullable.', self::class, $type));
+        }
+
+        if ($type instanceof UnionType) {
+            parent::__construct(Type::null(), ...$type->getTypes());
+
+            return;
+        }
+
+        parent::__construct(Type::null(), $type);
+    }
+
+    public function getWrappedType(): Type
+    {
+        return $this->type;
+    }
+
+    public function wrappedTypeIsSatisfiedBy(callable $specification): bool
+    {
+        return $this->getWrappedType()->isSatisfiedBy($specification);
+    }
+
+    public function isNullable(): bool
+    {
+        return true;
+    }
+}

Type/WrappingTypeInterface.php
@@ -1,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\TypeInfo\Type;
+
+use Symfony\Component\TypeInfo\Type;
+
+/**
+ * Represents a type wrapping another type.
+ *
+ * @author Mathias Arlaud <mathias.arlaud@gmail.com>
+ *
+ * @template T of Type
+ *
+ * @experimental
+ */
+interface WrappingTypeInterface
+{
+    /**
+     * @return T
+     */
+    public function getWrappedType(): Type;
+
+    /**
+     * @param callable(Type): bool $specification
+     */
+    public function wrappedTypeIsSatisfiedBy(callable $specification): bool;
+}