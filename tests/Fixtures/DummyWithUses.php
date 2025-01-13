195e00d84d463a6357913de2620638475b52434a
DerManoMann <mano@radebatz.net>
2025-01-13 13:30:47 +1300

    Initial import and setup

---
+++ tests/Fixtures/DummyWithUses.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Symfony\Component\TypeInfo\Tests\Fixtures;
+
+use Symfony\Component\TypeInfo\Type;
+use \DateTimeImmutable as DateTime;
+
+final class DummyWithUses
+{
+    private \DateTimeInterface $createdAt;
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getType(): Type
+    {
+        throw new \LogicException('Should not be called.');
+    }
+}
